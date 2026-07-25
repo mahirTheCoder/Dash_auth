18a726b9166ef2eb9797676d8f762049969080b3
mahirTheCoder <mahirchowdhury518@gmail.com>
2026-07-25 23:18:29 +0600

    changes

--- models/userSchema.js
+++ models/userSchema.js
@@ -41,6 +41,7 @@ const userSchema = new mongoose.Schema({
   role: {
     type: String,
     required: true,
+    trim: true,
     enum: ["student", "admin", "teacher"],
     default: "student",
   },
--- routes/dashboard_route/admin.js
+++ routes/dashboard_route/admin.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const { allUserCheck , approvedUserCheck , deleteUserCheck , studentUserCheck} = require("../../controllers/adminController");
+const { allUserCheck , approvedUserCheck , deleteUserCheck , studentUserCheck, teacherUserCheck} = require("../../controllers/adminController");
 
 
 
 router.get("/allUserCheck", allUserCheck);
 router.get("/studentUserCheck", studentUserCheck);
+router.get("/teacherUserCheck", teacherUserCheck);
 router.patch("/approvedUserCheck/:id", approvedUserCheck);
-router.delete("/deleteUserCheck/:id", deleteUserCheck);
 router.delete("/deleteUserCheck/:id", deleteUserCheck);
 
 module.exports = router;
