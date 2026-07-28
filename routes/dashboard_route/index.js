0ee1674aace5cd9933385f2eb597456957e0d1c6
mahirTheCoder <mahirchowdhury518@gmail.com>
2026-07-28 17:14:52 +0600

    subject schema add

--- routes/dashboard_route/index.js
+++ routes/dashboard_route/index.js
@@ -2,9 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 const adminRoutes = require("./admin");
+const subjectRoutes = require("./subject");
 
 
 
 router.use("/admin", adminRoutes);
+router.use("/subject", subjectRoutes);
 
 module.exports = router;
