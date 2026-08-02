49f203493b08c2edb22a1259e208d5b985966082
mahirTheCoder <mahirchowdhury518@gmail.com>
2026-08-02 16:03:53 +0600

    update

--- controllers/subjectController.js
+++ controllers/subjectController.js
@@ -41,6 +41,27 @@ const createSubject = async (req, res) => {
   }
 };
 
+// -----------get all subjects
+
+const getAllSubjects = async (req, res) => {
+  try {
+    const subjects = await subjectSchema.find();
+    return res.status(200).json({
+      success: true,
+      message: "Subjects retrieved successfully.",
+      data: subjects,
+    });
+  } catch (error) {
+    console.error("Error fetching subjects:", error);
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error.",
+    });
+  }
+};
+
+
 module.exports = {
   createSubject,
+  getAllSubjects,
 };
--- routes/dashboard_route/subject.js
+++ routes/dashboard_route/subject.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { createSubject } = require("../../controllers/subjectController");
+const { createSubject , getAllSubjects } = require("../../controllers/subjectController");
 const { authMiddleware } = require("../../middleware/authMiddleware");
 const { requireAdmin } = require("../../middleware/roleCheckMiddleware");
 
 
 router.post("/create" , authMiddleware, requireAdmin,  createSubject  );
+router.get("/getallsubjects" , authMiddleware, requireAdmin,  getAllSubjects  );
 
 module.exports = router;
