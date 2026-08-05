4bff63fe1c684e830609b62206993192a230f2c6
mahirTheCoder <mahirchowdhury518@gmail.com>
2026-08-05 23:55:42 +0600

    dns add

--- server.js
+++ server.js
@@ -6,15 +6,9 @@ const cookieParser = require("cookie-parser");
 const dbConfig = require('./configs/dbConfig')
 const PORT = process.env.PORT
 
-// const limiter = rateLimit({
-// 	windowMs: 10 * 60 * 1000, // 10 minutes
-// 	limit: 5, // Limit each IP to 5 requests per `window` (here, per 10 minutes).
-// 	standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
-// 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
-// 	ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
-// 	// store: ... , // Redis, Memcached, etc. See below.
-// })
-
+// // -----when db congfig not working then use this code t
+const dns = require('dns');
+dns.setServers(['8.8.8.8', '8.8.4.4'])
 
 const app = express()
 app.use(express.json())
@@ -23,10 +17,6 @@ app.use(router)
 // app.use(limiter)
 dbConfig()
 
-
-// // -----when db congfig not working then use this code t
-// // const dns = require('dns');
-// // dns.setServers(['8.8.8.8', '8.8.4.4'])
 
 
 app.listen(PORT, () => {
